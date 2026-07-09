839875ca9cb8f045c04eed3310cccf13e253f219
esemv07 <ruby.semaganda@gmail.com>
2026-07-09 13:57:33 +1000

    Create final character page with random features [features DO NOT stack yet]

--- script.js
+++ script.js
@@ -161,7 +161,7 @@ likesOrDislikes = [
     "comedy",
 ]
 
-character = {
+character = JSON.parse(localStorage.getItem("character")) || {
     "expression": "",
     "age": "",
     "birthMonth": "",
@@ -178,22 +178,84 @@ function getRandomItem(array, number) {
     return [...array].sort( () => Math.random() - 0.5).slice(0, number)
 }
 
-function pickItems(arrayName, number) {
-    if (arrayName == likesOrDislikes) {
-        const results = getRandomItem(arrayName, number);
+function pickItems(array, number, arrayName) {
+    if (array == likesOrDislikes) {
+        const results = getRandomItem(array, number);
         document.getElementById('next').classList.remove('hidden');
         let resultsString = "";
         resultsString = `Likes... ${results[0]}<br><br>Dislikes... ${results[1]}`;
         document.getElementById('results').innerHTML = resultsString;
+        character.likes = results[0];
+        character.dislikes = results[1];
+
+        localStorage.setItem("character", JSON.stringify(character));
     } else {
-        const results = getRandomItem(arrayName, number);
+        const results = getRandomItem(array, number);
         document.getElementById('next').classList.remove('hidden');
         let resultsString = "";
-        results.forEach((result) => {
-            resultsString += `${result}<br>`;
-        })
+        if (results.length > 1) {
+            results.forEach((result) => {
+                resultsString += `${result}<br>`;
+            })
+        } else {
+            results.forEach((result) => {
+                resultsString += `${result}`;
+            })
+        }
         document.getElementById('results').innerHTML = resultsString;
+        character[arrayName] = resultsString;
+
+        localStorage.setItem("character", JSON.stringify(character));
     }
     document.getElementById('resultsContainer').classList.remove('hidden');
     document.getElementById('picker').disabled = true;
 }
+
+
+function restart() {
+    localStorage.removeItem("character");
+}
+
+
+window.addEventListener('load', () => {
+    if (window.location.href.includes("character.html")) {
+        const character = JSON.parse(localStorage.getItem("character"));
+        document.getElementById('expression').innerHTML = character.expression;
+        document.getElementById('birthday').innerHTML = `Age: ${character.age}<br>Birth Month: ${character.birthday}`;
+        document.getElementById('height').innerHTML = character.height;
+        document.getElementById('strengthsAndWeaknesses').innerHTML = `Strengths:<br>${character.strengths}<br>Weaknesses:<br>${character.weaknesses}`;
+        document.getElementById('fears').innerHTML = character.fears;
+        document.getElementById('likesAndDislikes').innerHTML = `Likes...<br>${character.likes}<br><br>Dislikes...<br>${character.dislikes}`;
+
+        document.getElementById('head').src = `assets/${character.expression} character/${character.expression} Character Base.png`;
+
+        let skin = [
+            "Black",
+            "Brown",
+            "Tan",
+            "White",
+        ];
+
+        let hair = [
+            "Black",
+            "Blonde",
+            "Brown",
+            "Orange",
+        ];
+
+        let eyes = [
+            "Blue",
+            "Brown",
+            "Green",
+            "Grey",
+        ];
+
+        let randomSkin = Math.floor(Math.random() * 4);
+        let randomHair = Math.floor(Math.random() * 4);
+        let randomEyes = Math.floor(Math.random() * 4);
+
+        document.getElementById('skin').src = `assets/${character.expression} character/Skin ${skin[randomSkin]}.png`;
+        document.getElementById('hair').src = `assets/${character.expression} character/Hair ${hair[randomHair]}.png`;
+        document.getElementById('eyes').src = `assets/${character.expression} character/Eyes ${eyes[randomEyes]}.png`;
+    }
+})
